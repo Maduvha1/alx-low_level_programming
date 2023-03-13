e6f6d433dbe82031fe7b41c2f3dfb291e1757f1d
Maduvha1 <mtshindane@gmail.com>
2023-03-13 11:12:22 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
 *
 * Return: Always 0 (Success)
 */
-		
+
 int main(int argc, char *argv[])
 
 {
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 (void) argv; /*Ignore argv*/
 
 	printf("%d\n", argc - 1);
-		
+
 	return (0);
 
 }
